b98ea65721d14a7ac04f653352d35ad8f7dbab4b
John Kakouris <jdkakouris@gmail.com>
2023-10-23 16:45:38 +0200

    changes to display more details

--- index.js
+++ index.js
@@ -1,60 +1,56 @@
-/*
-let movieList = []
-
-function renderMovies() {
-  let html = ""
-  for (let movie of movieList){
-    html += `<h3>${movie}</h3>`
-  }
-  document.getElementById("bottom-section").innerHTML = html
-}
 
 
 
+document.getElementById("submit").addEventListener("click", async function (event) {
+  event.preventDefault();
 
-
-fetch("http://www.omdbapi.com/?t=Frozen&apikey=b9dda53d")
-.then(response => response.json())
-.then(data => {
-  movieList = data
-  renderMovies()
-})
-
-
-
-fetch("http://www.omdbapi.com/?t=Frozen&apikey=b9dda53d")
-.then(res => res.json())
-.then(data => {
-  document.getElementById("bottom-section").innerHTML = `<p>${data.Title}</p>`;
-})
-*/
-
-document.getElementById("submit").addEventListener("click", function(event){
-  event.preventDefault()
-
-  const searchQuery = document.getElementById("movie-search").value;  
+  const searchQuery = document.getElementById("movie-search").value;
   const url = `http://www.omdbapi.com/?s=${searchQuery}&apikey=b9dda53d`;
-  
-  fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    
-    const movieArray = data.Search.slice(0,3)
 
-    if (Array.isArray(movieArray)){
-      const content = movieArray.map((movie) => `
-        <div class="movie">
-          <img src="${movie.Poster}">
-          <p>Title: ${movie.Title}</p>
-          <p>Year: ${movie.Year}</p>
+  const response = await fetch(url);
+   const data = await response.json();
+
+  if (data.Response === "False") {
+    document.getElementById("bottom-section").innerHTML = `<h2>${data.Error}</h2>`;
+  }
+  else{
+    const movieArray = data.Search.slice(0, 3);
+
+    if (Array.isArray(movieArray)) 
+    {
+    const movieDetailsPromises = movieArray.map(async (movie) => {
+      const imdbID = movie.imdbID;
+      const movieDetailsUrl = `http://www.omdbapi.com/?i=${imdbID}&apikey=b9dda53d`;
+
+      const movieDetailsResponse = await fetch(movieDetailsUrl);
+      const movieDetailsData = await movieDetailsResponse.json();
+
+      return movieDetailsData;
+    });
+
+    const movieDetails = await Promise.all(movieDetailsPromises);
+
+    const movieDetailsHTML = movieDetails.map((movie) => `
+      <div class="movie">
+      <img src="${movie.Poster}">
+      <div class="movie-details">
+        <div class="movie-comp">
+          <p>${movie.Title}</p>
+          <i class="fa-solid fa-star" style="color: #f7c202;"></i>
+          <p>${movie.imdbRating}</p>
         </div>
-      `).join("")
+        <div class="movie-comp">
+          <p>${movie.Runtime}</p>
+          <p>${movie.Genre}</p>
+          <button>Watchlist</button>
+        </div>
+        <p>${movie.Plot}</p>
+      </div>
+    </div>
+    `).join('');
 
-      document.getElementById("bottom-section").innerHTML = content
+    document.getElementById("bottom-section").innerHTML = movieDetailsHTML;
+    console.log(movieDetails);
     }
-    if (data.Response === "False"){
-      document.getElementById("bottom-section").innerHTML = `<h2>${data.Error}</h2>`
-    }
-    console.log(data)
-  })
-})
+  }
+});
