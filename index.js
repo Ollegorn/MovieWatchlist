a619b241218a8b8a3a8bc30910af6d1b5eedeae7
John Kakouris <jdkakouris@gmail.com>
2023-10-24 02:15:27 +0200

    added saving to local storage func

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 
-
-
 document.getElementById("submit").addEventListener("click", async function (event) {
   event.preventDefault();
   const searchQuery = document.getElementById("movie-search").value;
@@ -12,11 +10,9 @@ document.getElementById("submit").addEventListener("click", async function (even
     document.getElementById("bottom-section").innerHTML = `<h2>${data.Error}</h2>`;
   }
   else{
-    const movieArray = data.Search.slice(0, 3);
+    const movieArray = data.Search.slice(0, 5);
 
-   
     const movieDetailsPromises = movieArray.map(async (movie) => {
-
       const movieDetailsResponse = await fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=b9dda53d`);
       const movieDetailsData = await movieDetailsResponse.json();
 
@@ -27,24 +23,42 @@ document.getElementById("submit").addEventListener("click", async function (even
 
     const movieDetailsHTML = movieDetails.map((movie) => `
       <div class="movie">
-      <img src="${movie.Poster}">
-      <div class="movie-details">
-        <div class="movie-comp">
-          <p>${movie.Title}</p>
-          <i class="fa-solid fa-star" style="color: #f7c202;"></i>
-          <p>${movie.imdbRating}</p>
+        <img src="${movie.Poster}">
+        <div class="movie-details">
+          <div class="movie-comp">
+            <p>${movie.Title}</p>
+            <i class="fa-solid fa-star" style="color: #f7c202;"></i>
+            <p>${movie.imdbRating}</p>
+          </div>
+          <div class="movie-comp">
+            <p>${movie.Runtime}</p>
+            <p>${movie.Genre}</p>
+            <button class="plus" data-imdb="${movie.imdbID}">➕Watchlist</button>
+          </div>
+          <p>${movie.Plot}</p>
         </div>
-        <div class="movie-comp">
-          <p>${movie.Runtime}</p>
-          <p>${movie.Genre}</p>
-          <button>Watchlist</button>
-        </div>
-        <p>${movie.Plot}</p>
       </div>
-    </div>
     `).join('');
 
     document.getElementById("bottom-section").innerHTML = movieDetailsHTML;
-    console.log(movieDetails);
+
+    const watchlistButtons = document.getElementsByClassName("plus");
+    for (const button of watchlistButtons){
+      button.addEventListener("click", function(){
+        const imdbID = this.getAttribute("data-imdb");
+        const movie = movieDetails.find((item) => item.imdbID === imdbID);
+
+        if (movie) {
+          let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
+
+          if (!watchlist.some((item) => item.imdbID === imdbID)){
+            watchlist.push(movie);
+            localStorage.setItem("watchlist", JSON.stringify(watchlist));
+            alert(`${movie.Title} has been added to your watchlist!`)
+          }
+        }
+      });
     }
+  }
 });
+
